aca5fb8fc813b52323ec3c06f84b42cc89ff2b24
Yariv Katz <yariv@nerdeez.com>
2013-11-24 16:03:24 +0200

    finished with the schedular

--- ticketz_backend_app/clock.py
+++ ticketz_backend_app/clock.py
@@ -7,12 +7,9 @@ sched = Scheduler()
 
 @sched.interval_schedule(minutes=1)
 def close_deals():
-    print 'This job is run every minute.'
+    print 'Closing deals that passed the valid_to'
     Deal.objects.filter(valid_to__lte=datetime.datetime.now()).update(status=3)
 
-@sched.cron_schedule(day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print 'This job is run every weekday at 5pm.'
 
 sched.start()
 
