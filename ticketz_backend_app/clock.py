adf6b3a0b59bda6d56933e6c21c0422fd2a26aef
Yariv Katz <yariv@nerdeez.com>
2013-11-25 16:25:43 +0200

    deleting the old api keys

--- ticketz_backend_app/clock.py
+++ ticketz_backend_app/clock.py
@@ -3,6 +3,8 @@
 from apscheduler.scheduler import Scheduler
 from ticketz_backend_app.models import *
 from dateutil.relativedelta import relativedelta
+from tastypie.models import ApiKey
+from tastypie.api import Api
 
 sched = Scheduler()
 
@@ -34,6 +36,19 @@ def activate_pending_deals():
     now = datetime.datetime.now()
     ten_before = now + relativedelta(minutes=-10)
     Deal.objects.filter(creation_date__lte=ten_before, status=1).update(status=2)
+    
+@sched.interval_schedule(hours=24)
+def delete_old_api_keys():
+    '''
+    api keys that are older than 24 hours should be deleted
+    '''
+    
+    print 'Deleting old api keys'
+    now = datetime.datetime.now()
+    day_before = now + relativedelta(hours=-24)
+    ApiKey.objects.filter(created__lt=day_before).delete()
+    
+
 
 
 sched.start()
