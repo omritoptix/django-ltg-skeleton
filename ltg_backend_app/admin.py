67bdfeee9778c744eabd18040323e4cfda41dc37
Omri Dagan <omritoptix@gmail.com>
2014-04-08 16:12:28 +0300

    added user profile to django admin interface

--- ltg_backend_app/admin.py
+++ ltg_backend_app/admin.py
@@ -10,7 +10,8 @@ Created on November 7, 2013
 #===============================================================================
 # begin imports
 #===============================================================================
-
+from django.contrib import admin
+from ltg_backend_app.models import *
 #===============================================================================
 # end imports
 #===============================================================================
@@ -19,7 +20,17 @@ Created on November 7, 2013
 # begin admin models
 #===============================================================================
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id','uuid','full_name','get_email')
+    search_fields=['user__email','user__first_name','user__last_name']
     
+    def full_name(self,user_profile):
+        return ("%s %s" % (user_profile.user.last_name, user_profile.user.first_name))
+    full_name.short_description = "Full Name"
+    
+    def get_email(self, user_profile):
+        return user_profile.user.email
+    get_email.short_description = "Email"    
 
 
 #===============================================================================
@@ -30,6 +41,7 @@ Created on November 7, 2013
 # begin admin site regitration
 #===============================================================================
 
+admin.site.register(UserProfile, UserProfileAdmin)
 
 #===============================================================================
 # end admin site registration
