b125bb9c8acacccd6bba806abf6a95ad041775a1
Omri Dagan <omritoptix@gmail.com>
2014-04-10 12:48:32 +0300

    disabled post save api key signal for test fixtures

--- ltg_backend_app/models.py
+++ ltg_backend_app/models.py
@@ -18,6 +18,7 @@ from django.contrib.auth.models import User
 import logging
 from tastypie.models import create_api_key
 from django.utils.timezone import utc
+from functools import wraps
 
 #===============================================================================
 # end imports
@@ -120,10 +121,30 @@ class Tutor(object):
 # begin signals
 #===============================================================================
 
+def disable_for_loaddata(signal_handler):
+    """
+    Decorator that turns off signal handlers when loading fixture data.
+    """
+    @wraps(signal_handler)
+    def wrapper(*args, **kwargs):
+        if kwargs['raw']:
+            return
+        signal_handler(*args, **kwargs)
+    return wrapper
+
+@disable_for_loaddata
+def create_api_key_wrapper(sender,**kwargs):
+    '''
+    will wrap the original create_api_key func in order
+    to prevent post save signal while using fixtures in testing since
+    causes integration error.
+    '''
+    create_api_key(sender,**kwargs)
+    
 # get a user profile for user, or create it if doesn't exist
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 # on user save, create an api key for it
-models.signals.post_save.connect(create_api_key, sender=User)
+models.signals.post_save.connect(create_api_key_wrapper, sender=User)
 
 #===============================================================================
 # end signals
